9f2924261c7f54185d5939ace2ca83081c6d9ee0
Kostas Evangelou <kostas.ev79@gmail.com>
2018-11-14 00:05:36 +0200

    read conf file

--- main.cpp
+++ main.cpp
@@ -1,6 +1,51 @@
 #include <iostream>
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+#include <unordered_map>
+#include <getopt.h>
+#include <cstring>
+#include <numeric>
+#include <cmath>
+#include <chrono>
+#include <random>
+#include <unistd.h>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+void get_cfg(int &, int &, int &);
+
+using namespace std;
+
+int main(int argc, char** argv) {
+    int num_clusters = 5;
+    int num_hfunc = 4;
+    int num_htables = 5;
+    get_cfg(num_clusters,num_hfunc,num_htables);
+
+
     return 0;
+}
+
+void get_cfg(int &clusters, int &hfunc, int &htables) {
+    ifstream file;
+    file.open ("../cluster.conf");
+    if (!file.is_open()){
+        cerr << "Failed to open cluster.conf file! " << endl;
+        exit(0) ;
+    }
+    string word;
+    while (file >> word)
+    {
+        if (word == "number_of_clusters:") {
+            file >> word;
+            clusters=stoi(word);
+        } else if (word == "number_of_hash_functions:") {
+            file >> word;
+            hfunc=stoi(word);
+        } else if (word == "number_of_hash_tables:") {
+            file >> word;
+            htables=stoi(word);
+        }
+
+    }
 }
