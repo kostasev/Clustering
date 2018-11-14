9cc29a3704477d342db75258a9adfc6b755ac73d
Kostas Evangelou <kostas.ev79@gmail.com>
2018-11-14 23:10:25 +0200

    interface done

--- main.cpp
+++ main.cpp
@@ -12,23 +12,57 @@
 #include <random>
 #include <unistd.h>
 
-void get_cfg(int &, int &, int &);
+void get_cfg(std::string ,int &, int &, int &);
 
 using namespace std;
 
 int main(int argc, char** argv) {
+    int c;
     int num_clusters = 5;
     int num_hfunc = 4;
     int num_htables = 5;
+    string input, conf, output, metric;
     get_cfg(num_clusters,num_hfunc,num_htables);
 
-
+    /* Reading Arguments from command line */
+    while ((c = getopt(argc, argv, "i:c:o:d")) != -1) {
+        switch (c) {
+            case 'i':
+                input = optarg;
+                break;
+            case 'c':
+                conf =  optarg;
+                break;
+            case 'o':
+                output = optarg;
+                break;
+            case 'd':
+                metric = optarg;
+                if ((metric != "cosine") && (metric != "euclidean")) {
+                    cerr << "Invalid function name" << endl;
+                }
+                break;
+            case 'h':
+                cout << "\nCluster\n" << endl <<
+                     "Mandatory options\n\t-i -c\n\n"
+                     "\t-i input file\n"
+                     "\t-c conf file\n"
+                     "\t-o output file\n"
+                     "\t-d metric" << endl;
+                return 0;
+            case '?':
+                cerr << "Unknown option character " << char(optopt) << endl;
+                return 1;
+            default:
+                abort();
+        }
+    }
     return 0;
 }
 
-void get_cfg(int &clusters, int &hfunc, int &htables) {
+void get_cfg(string inputf, int &clusters, int &hfunc, int &htables) {
     ifstream file;
-    file.open ("../cluster.conf");
+    file.open (inputf);
     if (!file.is_open()){
         cerr << "Failed to open cluster.conf file! " << endl;
         exit(0) ;
